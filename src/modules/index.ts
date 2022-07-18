e0058182f779d9972f708787b7edf53d9b583dd0
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-07-18 18:22:37 +0300

    feat: add description for images and import to index

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,8 +1,2 @@
 import '../assets/styles/styles.scss';
-
-// TODO delete message
-alert(`Проверьте, пожалуйста, работу в четверг 21.07.2022, ибо я немного не успел. Пожалуйста! 
-Мои контакты: 
-Discord: MatsveiDubaleka#1377
-Telegram: @MatsveiDubaleka
-Буду очень благодарен, если вы проверите в Четверг, пожалуйста!`);
+import './data/data';
