88bb5bffb1f62dae2384ad80583be4bf766f7c8c
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-07-18 18:16:50 +0300

    feat: add required rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,7 @@ module.exports = {
     },
   },
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'on',
   },
   settings: {
     react: {
