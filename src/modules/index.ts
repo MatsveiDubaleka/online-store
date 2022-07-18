0d4aeebc1782b50a7f84bd79a947bbd86038c474
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-07-18 14:32:58 +0300

    refactor: add basic markup

---
+++ src/modules/index.ts
@@ -0,0 +1,8 @@
+import '../assets/styles/styles.scss';
+
+// TODO delete message
+console.log(`Проверьте, пожалуйста, работу в четверг 21.07.2022, ибо я немного не успел. Пожалуйста! 
+Мои контакты: 
+Discord: MatsveiDubaleka#1377
+Telegram: @MatsveiDubaleka
+Буду очень благодарен, если вы проверите в Четверг, пожалуйста!`);
