4f046df11ca571e47386380cc4d917b68b143e39
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-07-18 16:11:11 +0300

    refactor: change warning message

---
+++ dist/main.65751c2da5b626a7d805.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";alert("Проверьте, пожалуйста, работу в четверг 21.07.2022, ибо я немного не успел. Пожалуйста! \nМои контакты: \nDiscord: MatsveiDubaleka#1377\nTelegram: @MatsveiDubaleka\nБуду очень благодарен, если вы проверите в Четверг, пожалуйста!")})();
+//# sourceMappingURL=main.65751c2da5b626a7d805.js.map
--- dist/main.aa762909ef43ab2ba37c.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";console.log("Проверьте, пожалуйста, работу в четверг 21.07.2022, ибо я немного не успел. Пожалуйста! \nМои контакты: \nDiscord: MatsveiDubaleka#1377\nTelegram: @MatsveiDubaleka\nБуду очень благодарен, если вы проверите в Четверг, пожалуйста!")})();
-//# sourceMappingURL=main.aa762909ef43ab2ba37c.js.map
