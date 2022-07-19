8f77131046bb6427645708f99c59bf7c6a222630
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-07-19 22:53:34 +0300

    feat: add generating data items to page

---
+++ src/modules/generation/generation.ts
@@ -0,0 +1,60 @@
+import { IData } from '../data/IData';
+import data from '../data/data';
+
+const imageContainer = document.querySelector(
+  '.image-container'
+) as HTMLElement;
+
+data.forEach((toy: IData) => {
+  const imageItem = document.createElement('div');
+  imageItem.className = 'image-item';
+  imageContainer.appendChild(imageItem);
+
+  const toyImage = document.createElement('img');
+  toyImage.src = `../assets/images/${toy.num}.png`;
+  imageItem.appendChild(toyImage);
+
+  const toyName = document.createElement('p');
+  toyName.className = 'toy-name';
+  toyName.innerText = `${toy.name}`;
+  imageItem.appendChild(toyName);
+
+  const toyYear = document.createElement('p');
+  toyYear.className = 'toy-year';
+  toyYear.innerText = `Год выпуска: ${toy.year} г.`;
+  imageItem.appendChild(toyYear);
+
+  const toyShape = document.createElement('p');
+  toyShape.className = 'toy-shape';
+  toyShape.innerText = `Форма: ${toy.shape}`;
+  imageItem.appendChild(toyShape);
+
+  const toyCount = document.createElement('p');
+  toyCount.className = 'toy-count';
+  toyCount.innerText = `Осталось: ${toy.count} шт.`;
+  imageItem.appendChild(toyCount);
+
+  const toyColor = document.createElement('p');
+  toyColor.className = 'toy-color';
+  toyColor.innerText = `Цвет: ${toy.color}`;
+  imageItem.appendChild(toyColor);
+
+  const toySize = document.createElement('p');
+  toySize.className = 'toy-size';
+  toySize.innerText = `Размер: ${toy.size}`;
+  imageItem.appendChild(toySize);
+
+  const btnBasket = document.createElement('button');
+  btnBasket.className = 'btn-basket';
+  btnBasket.innerText = 'Добавить в корзину';
+  imageItem.appendChild(btnBasket);
+
+  const basketState = document.createElement('p');
+  basketState.className = 'basket-state';
+  if (basketState.classList.contains('basket-open')) {
+    basketState.innerText = 'Товар в корзине';
+  } else {
+    basketState.innerText = 'Товар не в корзине';
+  }
+  imageItem.appendChild(basketState);
+});
