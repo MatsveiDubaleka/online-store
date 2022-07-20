9a1ba028c520612f1678a52c1f3f1332e668e3dc
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-07-20 11:52:20 +0300

    feat: add number of items in busket

--- src/modules/basket/basket.ts
+++ src/modules/basket/basket.ts
@@ -1,6 +1,8 @@
 // Adding to Basket Items
 
 const btnsAddBasket = document.querySelectorAll('.btn-basket');
+const basketNum = document.querySelector('.basket-number') as HTMLElement;
+let basketItems = 0;
 
 btnsAddBasket.forEach((btn) => {
   btn.addEventListener('click', () => {
@@ -10,10 +12,14 @@ btnsAddBasket.forEach((btn) => {
     if (btn.innerHTML === 'Добавить в корзину') {
       btn.innerHTML = 'Удалить из корзины';
       btn.classList.add('cart-success');
+      basketItems += 1;
+      basketNum.innerText = `${basketItems}`;
       basketState.innerHTML = 'Товар в Корзине';
     } else if (btn.innerHTML === 'Удалить из корзины') {
       btn.classList.remove('cart-success');
       btn.innerHTML = 'Добавить в корзину';
+      basketItems -= 1;
+      basketNum.innerText = `${basketItems}`;
       basketState.innerHTML = 'Товар не в Корзине';
     }
   });
