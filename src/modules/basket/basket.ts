130d65b9cc7e893ad9e5462b06f4afb3b740b922
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-07-20 12:40:21 +0300

    feat: add function of warning

--- src/modules/basket/basket.ts
+++ src/modules/basket/basket.ts
@@ -6,21 +6,35 @@ let basketItems = 0;
 
 btnsAddBasket.forEach((btn) => {
   btn.addEventListener('click', () => {
-    const basketState = btn.parentElement?.querySelector(
+    const basketState = (btn.parentElement as HTMLElement).querySelector(
       '.basket-state'
     ) as HTMLElement;
     if (btn.innerHTML === 'Добавить в корзину') {
+      if (basketItems >= 20) {
+        basketWarning();
+        return;
+      }
       btn.innerHTML = 'Удалить из корзины';
       btn.classList.add('cart-success');
       basketItems += 1;
       basketNum.innerText = `${basketItems}`;
       basketState.innerHTML = 'Товар в Корзине';
+      (btn.parentElement as HTMLElement).classList.add('cart-success');
     } else if (btn.innerHTML === 'Удалить из корзины') {
       btn.classList.remove('cart-success');
       btn.innerHTML = 'Добавить в корзину';
       basketItems -= 1;
       basketNum.innerText = `${basketItems}`;
       basketState.innerHTML = 'Товар не в Корзине';
+      (btn.parentElement as HTMLElement).classList.remove('cart-success');
     }
   });
 });
+
+function basketWarning() {
+  const modalWarn = document.querySelector('.modal-warning') as HTMLElement;
+  modalWarn.classList.add('active-warning');
+  setTimeout(() => {
+    modalWarn.classList.remove('active-warning');
+  }, 5000);
+}
